f009696356df978433e48f6d924cf77a83c2ae64
CDDiaz <cabj_xeneize@hotmail.com>
2021-08-17 11:48:03 +1000

    login changes

--- src/components/registrations/Login.js
+++ src/components/registrations/Login.js
@@ -2,92 +2,11 @@ import React, {Component} from 'react';
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 
-// class Login extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state ={
-//       email: '',
-//       password: '',
-//       errors: ''
-//     };
-//   }
-//
-//   handleChange = (event) => {
-//     const{ name, value } = event.target
-//     this.setState({
-//       [name]: value
-//     })
-//   };
-//
-//   handleSubmit = (event) => {
-//       event.preventDefault()
-//       const { email, password } = this.state
-//       let user = {
-//         email: email,
-//         password: password
-//       }
-//
-//     axios.post('http://localhost:3001/login', {user},{ withCredientials:true })
-//    .then(response =>{
-//      console.log('fetching')
-//       if(response.data.logged_in){
-//         this.props.handleLogin(response.data.user)
-//         this.redirect() //if successful redirect to create page
-//       }else{
-//         this.setState({
-//           errors: response.data.errors
-//         })
-//       }
-//     })
-//     .catch(error => console.log('api errors:', error))
-//   };
-//
-//   redirect(){
-//     console.log('redirecting')
-//     this.props.history.push('/create')
-//   }
-//
-//   _handleErrors(){
-//     return(
-//       <div>
-//         <ul>
-//         {this.state.errors.map(error => {
-//           return <li key={error}>{error}</li>
-//         })}
-//         </ul>
-//       </div>
-//     )
-//   };
-//
-//   render(){
-//     return(
-//       <div>
-//       <br/>
-//       <div className = "home">
-//         <h3>Login</h3>
-//         <div className = "form">
-//           <form onSubmit={this.handleSubmit}>
-//             Email:<input type="text" onChange={this.handleChange}/>
-//             <br/>
-//             Password:<input type="password" onChange={this.handleChange}/>
-//             <br/>
-//             <button type="submit">Log in</button>
-//             <div>
-//             <p><small>Don't have an account? <Link to="/sign-up"> Sign up</Link></small></p>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//       )
-//   }
-// }
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
       email: '',
       password: '',
       errors: ''
@@ -96,17 +15,16 @@ class Login extends Component {
   componentWillMount() {
     return this.props.loggedInStatus ? this.redirect() : null
   }
-handleChange = (event) => {
+  handleChange = (event) => {
     const {name, value} = event.target
     this.setState({
       [name]: value
     })
   };
-handleSubmit = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault()
-    const {username, email, password} = this.state
-let user = {
-      username: username,
+    const {email, password} = this.state
+    let user = {
       email: email,
       password: password
     }
@@ -124,10 +42,11 @@ let user = {
     })
     .catch(error => console.log('api errors:', error))
   };
-redirect = () => {
+
+  redirect = () => {
     this.props.history.push('/')
   }
-handleErrors = () => {
+  handleErrors = () => {
     return (
       <div>
         <ul>
@@ -139,19 +58,13 @@ handleErrors = () => {
       </div>
     )
   }
-render() {
-    const {username, email, password} = this.state
-return (
+
+  render() {
+    const {email, password} = this.state
+    return (
       <div>
         <h1>Log In</h1>
         <form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-          />
           <input
             placeholder="email"
             type="text"
@@ -170,7 +83,7 @@ return (
             Log In
           </button>
           <div>
-            or <Link to='/signup'>sign up</Link>
+            <p><small>Don't have an account? <Link to="/signup"> Sign up</Link></small></p>
           </div>
 
           </form>
--- src/components/registrations/Signup.js
+++ src/components/registrations/Signup.js
@@ -2,90 +2,6 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 
-
-// class Signup extends Component {
-//   constructor(props){
-//     super(props);
-//     this.state={
-//       name: '',
-//       email: '',
-//       password:'',
-//       confirm: '',
-//       errors: ''
-//     };
-//   }
-//   handleChange = (event) => {
-//     const{ name, value } = event.target
-//     this.setState({
-//       [name]: value
-//     })
-//   };
-//
-//   _handleSubmit(e){
-//     e.preventDefault();
-//     const {name, email, password, confirm} = this.state
-//     let user = {
-//       name: name,
-//       email: email,
-//       password: password,
-//       confirm: confirm
-//     }
-//
-//     axios.post('http://localhost:3001/login', {user}, {withCredientials:true}).then(response =>{
-//       if(response.data.status === 'created'){
-//         this.props.handleLogin(response.data)
-//         this.redirect() //if successful redirec to home page
-//       }else{
-//         this.setState({
-//           errors: response.data.errors
-//         })
-//       }
-//     })
-//     .catch(error => console.log('api errors:', error))
-//   };
-//
-//   redirect = () => {
-//     this.props.history.push('/')
-//   }
-//
-//   handleErrors= () => {
-//     return(
-//       <div>
-//         <ul>
-//         {this.state.errors.map(error => {
-//           return <li key={error}>{error}</li>
-//         })}
-//         </ul>
-//       </div>
-//     )
-//   }
-//
-//   render(){
-//     return(
-//       <div>
-//       <br/>
-//       <div className = "home">
-//         <h3>Sign up</h3>
-//         <div className = "form">
-//           <form onSubmit={this.handleSubmit}>
-//             Name: <input type="text"
-//             onChange={this.handleChange}/>
-//             <br/>
-//             Email:<input type="text" onChange={this.handleChange}/>
-//             <br/>
-//             Password:<input type="password" onChange={this.handleChange}/>
-//             <br/>
-//             Confirm password: <input type="password" onChange={this.handleChange}/>
-//             <br/>
-//             <button type="submit">Register</button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//     )
-//   }
-// }
-
 class Signup extends Component {
   constructor(props) {
     super(props);
@@ -97,13 +13,15 @@ class Signup extends Component {
       errors: ''
      };
   }
-handleChange = (event) => {
+
+  handleChange = (event) => {
     const {name, value} = event.target
     this.setState({
       [name]: value
     })
   };
-handleSubmit = (event) => {
+
+  handleSubmit = (event) => {
     event.preventDefault()
     const {username, email, password, password_confirmation} = this.state
     let user = {
@@ -112,7 +30,7 @@ handleSubmit = (event) => {
       password: password,
       password_confirmation: password_confirmation
     }
-axios.post('http://localhost:3001/users', {user}, {withCredentials: true})
+    axios.post('http://localhost:3001/users', {user}, {withCredentials: true})
     .then(response => {
       if (response.data.status === 'created') {
         this.props.handleLogin(response.data)
@@ -125,10 +43,12 @@ axios.post('http://localhost:3001/users', {user}, {withCredentials: true})
     })
     .catch(error => console.log('api errors:', error))
   };
-redirect = () => {
+
+  redirect = () => {
     this.props.history.push('/')
   }
-handleErrors = () => {
+
+  handleErrors = () => {
     return (
       <div>
         <ul>{this.state.errors.map((error) => {
@@ -137,14 +57,15 @@ handleErrors = () => {
       </div>
     )
   }
-render() {
+  
+  render() {
     const {username, email, password, password_confirmation} = this.state
-return (
+    return (
       <div>
         <h1>Sign Up</h1>
         <form onSubmit={this.handleSubmit}>
           <input
-            placeholder="username"
+            placeholder="Name"
             type="text"
             name="username"
             value={username}
