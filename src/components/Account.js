99310343fcfd2e5cb1bd5da77c8ff14c6477ffd0
CDDiaz <cabj_xeneize@hotmail.com>
2021-08-20 13:10:45 +1000

    merge test

--- src/components/Account.js
+++ src/components/Account.js
@@ -61,7 +61,7 @@ componentDidMount() {
           <p>Email: <small> {this.props.user && this.props.user.email}</small></p>
 
           <p>Saved characters:</p>
-          <CharacterResults character={this.state.character} click={this._handleClick} />
+          <CharacterResults character={this.state.character} fetchCharacters={this.fetchCharacters} />
             <div>
             <p>{}</p>
             <p>{}</p>
